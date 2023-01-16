322c111e6a4ba66640464eb327b0a350340d30e6
Nathan Wilson <nathanwilson@nwilson.attlocal.net>
2023-01-16 17:39:37 -0600

    Initial setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import { useEffect } from 'react'
+
 import './App.css';
 
+const rectanglesOnMount = [{ x: 5, y: 5, width: 250, height: 350 }]
+
+const drawRectangle = ({ x, y, width, height }) => {
+  const canvas = document.getElementById("rectangle-editor")
+  const ctx = canvas.getContext("2d")
+  ctx.beginPath();
+  ctx.lineWidth = "6";
+  ctx.strokeStyle = "red";
+  ctx.fillStyle = "red"
+  ctx.rect(x, y, width, height);
+  ctx.fill();
+}
+
 function App() {
+  useEffect(() => {
+    rectanglesOnMount.map(drawRectangle)
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <canvas id="rectangle-editor" width="1280" height="720" />
     </div>
   );
 }
