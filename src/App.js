9eabc0e18e5acad8d88566ae1995d4dc91e601e2
Nathan Wilson <nathanwilson@nwilson.attlocal.net>
2023-01-16 18:29:36 -0600

    Establish pattern for saving rectangles

--- src/App.js
+++ src/App.js
@@ -2,21 +2,38 @@ import { useEffect } from "react"
 
 import "./App.css"
 
+const canvasId = "rectangle-editor"
+
 const rectanglesOnMount = [
-  { x: 5, y: 5, width: 250, height: 350, color: "red" },
+  { x: 5, y: 5, width: 0.3, height: 0.2, color: "red" },
 ]
 
+const getCanvasContext = () => {
+  const canvas = document.getElementById(canvasId)
+  return canvas.getContext("2d")
+}
+
+const setupCanvas = () => {
+  const ctx = getCanvasContext()
+  ctx.scale(1, 1)
+}
 const drawRectangle = ({ x, y, width, height, color }) => {
-  const canvas = document.getElementById("rectangle-editor")
-  const ctx = canvas.getContext("2d")
+  const ctx = getCanvasContext()
+  const actualWidth = ctx.canvas.clientWidth * width
+  const actualHeight = ctx.canvas.clientHeight * height
   ctx.beginPath()
   ctx.fillStyle = color
-  ctx.rect(x, y, width, height)
+  ctx.rect(x, y, actualWidth, actualHeight)
   ctx.fill()
 }
 
+const canvasHeight = window.innerHeight * 0.8
+const canvasWidth = window.innerWidth * 0.8
+
 function App() {
+  // Need to handle resizing
   useEffect(() => {
+    setupCanvas()
     rectanglesOnMount.map(drawRectangle)
   }, [])
 
@@ -32,8 +49,10 @@ function App() {
       className="App"
     >
       <canvas
-        style={{ border: "1px solid black", height: "80%", width: "80%" }}
-        id="rectangle-editor"
+        style={{ border: "1px solid black" }}
+        height={canvasHeight}
+        width={canvasWidth}
+        id={canvasId}
       />
     </div>
   )
